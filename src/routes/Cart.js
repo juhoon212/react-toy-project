6a13f1d18dfe0fd9d33b3e0beb5fc908c8fa65f9
juhoon212 <ws5501@naver.com>
2024-02-07 22:14:49 +0900

    redux

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,8 +1,19 @@
 import {Table} from 'react-bootstrap'
+import {useDispatch, useSelector} from "react-redux";
+import {changeName} from "../store";
 
 function Cart() {
+
+  const store = useSelector((state) => {
+    return state;
+  });
+  let dispatch = useDispatch();
+
   return (
     <div>
+
+      {store.user}의 장바구니
+
       <Table>
         <thead>
           <tr>
@@ -13,12 +24,23 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+        {
+          store.items.map((item, index) => {
+            return (
+                <tr>
+                  <td>{store.items[index].id}</td>
+                  <td>{store.items[index].name}</td>
+                  <td>{store.items[index].count}</td>
+                  <td>
+                    <button onClick={() => {
+                      dispatch(changeName())
+                    }
+                    }>+</button>
+                  </td>
+                </tr>
+            )
+          })
+        }
         </tbody>
       </Table> 
     </div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {configureStore, createSlice} from "@reduxjs/toolkit";
+
+
+let user = createSlice({
+    name : 'user',
+    initialState : 'kim',
+    reducers : {
+        changeName(state) {
+            return 'john' + state;
+        }
+    }
+})
+
+export let {changeName} = user.actions
+
+let stock = createSlice({
+    name: 'stock',
+    initialState: [10, 11, 12]
+});
+
+let items = createSlice({
+    name : 'items',
+    initialState : [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+    ]
+})
 
 export default configureStore({
-  reducer: { }
-}) 
+    reducer : {
+        user : user.reducer,
+        stock : stock.reducer,
+        items : items.reducer
+    }
+})
+
+
+
